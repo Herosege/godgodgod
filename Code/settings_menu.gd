3ba30617c1910e4859e876b93d75deb8a2977fef
Herosege <marcingajor70@gmail,com>
2024-10-29 16:36:51 +0100

    added enemies

---
+++ Code/settings_menu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var VSyncBut = $Settings/MarginContainer/VBoxContainer/HBoxContainer3/VSyncButton
+@onready var AVSl = $Settings/MarginContainer/VBoxContainer/HBoxContainer5/AVSl
+@onready var SEVSl = $Settings/MarginContainer/VBoxContainer/HBoxContainer6/AVSl2
+
+func _ready():
+	VSyncBut.button_pressed = Globals.VSync
+	AVSl.value = Globals.MVol
+	SEVSl.value = Globals.SEVol
+
+func _on_exit_button_pressed():
+	Globals.save_game("user://config.conf",{
+	"VSync":Globals.VSync,
+	"MusicVolume":Globals.MVol,
+	"SoundEffectVolume":Globals.SEVol
+	})
+	visible = false
+
+func _on_v_sync_button_toggled(toggled_on):
+	Globals.VSync = toggled_on
+
+func _on_av_sl_value_changed(value):
+	$Settings/MarginContainer/VBoxContainer/HBoxContainer5/Label2.text = str(value)
+	Globals.MVol = value
+
+func _on_av_sl_2_value_changed(value):
+	$Settings/MarginContainer/VBoxContainer/HBoxContainer6/Label2.text = str(value)
+	Globals.SEVol = value
